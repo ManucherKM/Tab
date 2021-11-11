d632123b6cb26cbb766a7d85965bd15dd3780696
ManucherKM <91463158+ManucherKM@users.noreply.github.com>
2021-11-11 11:41:15 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const tabs = document.getElementById('tabs');
 const content = document.querySelectorAll('.content');
 
 const changeClass = el => {
-    for (let i = 0; i < tabs.children.leigth; i++) {
+    for (let i = 0; i < tabs.children.length; i++) {
         tabs.children[i].classList.remove('active');
     }
     el.classList.add('active');
@@ -11,7 +11,7 @@ const changeClass = el => {
 tabs.addEventListener('click', e => {
     const currTab = e.target.dataset.btn;
     changeClass(e.target);
-    for (let i = 0; i < content.leigth; i++) {
+    for (let i = 0; i < content.length; i++) {
         content[i].classList.remove('active');
         if(content[i].dataset.content === currTab) {
             content[i].classList.add('active');
